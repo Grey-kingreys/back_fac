838b351f9c9f662f657081e1e57b82dc33f3d35e
Souleymane Diallo <soulmamoudou0@gmail.com>
2026-05-19 19:59:56 +0000

    fix(accounts): fix isort import order in seed_superadmin

--- apps/accounts/management/commands/seed_superadmin.py
+++ apps/accounts/management/commands/seed_superadmin.py
@@ -1,6 +1,8 @@
 import os
-from django.core.management.base import BaseCommand
+
 from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
 from apps.accounts.models import Role
 
 User = get_user_model()
